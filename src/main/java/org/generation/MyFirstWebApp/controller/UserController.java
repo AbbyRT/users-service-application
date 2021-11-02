ce95792d5c8822dc9995817ac7f3b539129ce421
Abigail Romero <romerot.abigail@gmail.com>
2021-11-01 20:20:35 -0600

    Proyecto con JAVA y Spring, se usa @getMapping para desplegar datos en pantalla

---
+++ src/main/java/org/generation/MyFirstWebApp/controller/UserController.java
@@ -0,0 +1,41 @@
+package org.generation.MyFirstWebApp.controller;
+
+import org.generation.MyFirstWebApp.model.User;
+import org.generation.MyFirstWebApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController
+{
+    final UserService userService;
+
+    public UserController( @Autowired UserService userService )
+    {
+        this.userService = userService;
+        userService.add( new User( 1, "Santiago", "Carrillo" ) );
+        userService.add( new User( 2, "Abigail", "CRomero" ) );
+        userService.add( new User( 3, "Estuardo", "Perez" ) );
+    }
+
+    @GetMapping( "/user" )
+    public User getUser()
+    {
+        return userService.getUser( 0 );
+    }
+
+    @GetMapping("/users/all") //devolvera el numero de elementos de la lista
+    public int totalUsers(){
+        return userService.size();
+    }
+
+    @GetMapping("/users/names")
+    public List<User> getUsers(){
+        return userService.getAll();
+    }
+
+}
